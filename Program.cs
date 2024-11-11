3b4667426eadfa83571e18a4d4519375536ac884
llytaii <llytaii@pm.me>
2024-11-11 20:07:00 +0100

    its alive ~

--- Program.cs
+++ Program.cs
@@ -1,20 +1,82 @@
-int delayNextLetter = 200;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
 
-if (args.Any() && int.TryParse(args[0], out int val))
-    delayNextLetter = val;
-
-foreach (char c in GenerateNextLetter())
+public class Program
 {
-    Console.Write(c);
-    await Task.Delay(delayNextLetter);
-}
+    public class OutputChar
+    {
+        public char Char { get; set; }
+        public bool IsPartOfWord { get; set; }
 
-// Generator
-IEnumerable<char> GenerateNextLetter()
-{
-    string alphabet = "abcdefghijklmnopqrstuvwxyz";
-    Random random = new Random();
+        public OutputChar(char c)
+        {
+            Char = c;
+            IsPartOfWord = false;
+        }
+    }
 
-    while (true)
-        yield return alphabet[random.Next(alphabet.Length)];
+    public static void Main(string[] args)
+    {
+        int nextCharDelay = 300;
+        if (args.Any() && int.TryParse(args[0], out int val))
+            nextCharDelay = val;
+
+        // Load the word list into a HashSet for efficient lookups
+        var wordSet = new HashSet<string>(File.ReadLines("words2.txt").Select(s => s.Trim()));
+        int maxWordLength = wordSet.Max(word => word.Length);
+
+        List<OutputChar> buffer = new List<OutputChar>();
+
+        Random random = new Random();
+        const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        while (buffer.Count < maxWordLength)
+        {
+            char newChar = alphabet[random.Next(alphabet.Length)];
+            buffer.Add(new OutputChar(newChar));
+        }
+
+        while (true)
+        {
+            // Detect words in the buffer
+            for (int start = 0; start < buffer.Count; start++)
+            {
+                int maxLength = Math.Min(maxWordLength, buffer.Count - start);
+                for (int length = 1; length <= maxLength; length++)
+                {
+                    string substring = new string(buffer.Skip(start).Take(length).Select(c => c.Char).ToArray());
+                    if (wordSet.Contains(substring))
+                    {
+                        for (int k = start; k < start + length; k++)
+                        {
+                            buffer[k].IsPartOfWord = true;
+                        }
+                    }
+                }
+            }
+
+            // Output the first character with appropriate color
+            OutputChar outputChar = buffer[0];
+
+            if (outputChar.IsPartOfWord)
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            else
+                Console.ResetColor();
+
+            Console.Write(outputChar.Char);
+            Console.ResetColor();
+
+            // Remove the first character from the buffer
+            buffer.RemoveAt(0);
+
+            // Add a new random character to the buffer
+            char newChar = alphabet[random.Next(alphabet.Length)];
+            buffer.Add(new OutputChar(newChar));
+
+            Thread.Sleep(nextCharDelay);
+        }
+    }
 }
